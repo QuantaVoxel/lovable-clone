67f22a92a7beec523c333c3f17d2f107ba66a8d3
QuantaVoxel <developer@quantavoxel.digital>
2026-06-13 01:43:48 +0800

    setup project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    sri: {
+      algorithm: 'sha256',
+    },
+    useLightningcss: true,
+    lightningCssFeatures: {
+      include: ['light-dark', 'oklab-colors'],
+      exclude: ['nesting'],
+    },
+  },
+  turbopack: {
+    root: path.join(__dirname, '..'),
+    ignoreIssue: [
+      { path: '**/vendor/**' },
+      { path: 'app/**', title: 'Module not found' },
+      { path: /generated\/.*\.ts/, description: /expected error/i },
+    ],
+  },
 };
 
 export default nextConfig;
